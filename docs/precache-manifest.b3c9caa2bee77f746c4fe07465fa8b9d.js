e499bc6fbc35e6590b53fd9a9073f8a6ec0fd796
Manuel Esmerio <manuel_esmerio@hotmail.com>
2020-11-25 08:18:22 -0600

    Docs Agregados

---
+++ docs/precache-manifest.b3c9caa2bee77f746c4fe07465fa8b9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1556f87c0c1170b58748702f4677a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bad933f38fb518e6b14c",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "af055f1a3e28103e168f",
+    "url": "/static/css/main.b2974ec4.chunk.css"
+  },
+  {
+    "revision": "bad933f38fb518e6b14c",
+    "url": "/static/js/2.78d4ef12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78d4ef12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af055f1a3e28103e168f",
+    "url": "/static/js/main.143f20f4.chunk.js"
+  },
+  {
+    "revision": "2e1ba1d9b170109bbbe8",
+    "url": "/static/js/runtime-main.4a7841de.js"
+  }
+]);
